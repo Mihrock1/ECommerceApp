3824afa6a47202f4c3159f577a01675b30f0951e
Mihrock1 <mihir.atrey@gmail.com>
2023-09-30 05:00:30 -0400

    commit 9

--- e-commerce-app-ui/src/components/users/Dashboard.js
+++ e-commerce-app-ui/src/components/users/Dashboard.js
@@ -1,47 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-} from "mdb-react-ui-kit";
+import { MDBContainer } from "mdb-react-ui-kit";
 import { baseUrl } from "../Constants";
+import Product from "./Product";
+import { MDBRow, MDBCol } from "mdb-react-ui-kit";
 
 export default function Dashboard() {
   const location = useLocation();
-  
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const userId = {"id": location.state.id};
+    const userId = { id: location.state.id };
 
     fetch(baseUrl + "/Products/viewProducts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userId),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userId),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.statusCode === 200) {
+          setProducts(data.listProducts);
+          console.log(products);
+        }
       })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          if (data.statusCode === 200) {
-            setProducts(data.listProducts);
-            console.log(products);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+      .catch((err) => {
+        console.log(err);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.state.id]);
 
   return (
-    <MDBContainer fluid>
-      <MDBRow className="justify-content-center mb-0">
-        <MDBCol md="12" xl="10">
-            
-        </MDBCol>
+    <MDBContainer className="p-4 overflow-hidden">
+      <MDBRow className="gx-4 gy-4">
+        {products.map((product) => (
+          <MDBCol md="4">
+            <Product key={product.id} product={product} />
+          </MDBCol>
+        ))}
       </MDBRow>
     </MDBContainer>
   );
--- e-commerce-app-ui/src/components/users/Product.js
+++ e-commerce-app-ui/src/components/users/Product.js
@@ -1,55 +1,27 @@
 import React from "react";
 import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
   MDBCard,
   MDBCardBody,
   MDBCardImage,
   MDBCardTitle,
-  MDBIcon,
 } from "mdb-react-ui-kit";
 
 function Product(props) {
+  console.log(props);
   return (
-    <MDBContainer fluid className="my-5">
-      <MDBRow className="justify-content-center">
-        <MDBCol md="6">
-          <MDBCard className="text-black">
-            <MDBIcon fab icon="apple" size="lg" className="px-3 pt-3 pb-2" />
-            <MDBCardImage
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/3.webp"
-              position="top"
-              alt="Apple Computer"
-            />
-            <MDBCardBody>
-              <div className="text-center">
-                <MDBCardTitle>Believing is seeing</MDBCardTitle>
-                <p className="text-muted mb-4">Apple pro display XDR</p>
-              </div>
-              <div>
-                <div className="d-flex justify-content-between">
-                  <span>Pro Display XDR</span>
-                  <span>$5,999</span>
-                </div>
-                <div className="d-flex justify-content-between">
-                  <span>Pro stand</span>
-                  <span>$999</span>
-                </div>
-                <div className="d-flex justify-content-between">
-                  <span>Vesa Mount Adapter</span>
-                  <span>$199</span>
-                </div>
-              </div>
-              <div className="d-flex justify-content-between total font-weight-bold mt-4">
-                <span>Total</span>
-                <span>$7,197.00</span>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <MDBCard className="text-black">
+      <MDBCardImage src={props.product.imageUrl} className='img rounded mx-auto d-block' />
+      <MDBCardBody>
+        <div className="text-center">
+          <MDBCardTitle>{props.product.name}</MDBCardTitle>
+          <p className="text-muted mb-4">{props.product.manufacturer}</p>
+        </div>
+        <div className="d-flex justify-content-between">
+          <span>{props.product.unitPrice}</span>
+          <span>{props.product.discount}</span>
+        </div>
+      </MDBCardBody>
+    </MDBCard>
   );
 }
 
